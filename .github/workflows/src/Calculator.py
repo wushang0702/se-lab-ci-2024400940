00bd8ba6e41567e7721ec27c1ad6743604e8ed09
wushang0702 <753769298@qq.com>
2026-05-13 17:28:15 +0800

    Add files via upload

---
+++ .github/workflows/src/Calculator.py
@@ -0,0 +1,15 @@
+# Calculator.py 计算器类
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("除数不能为0")
+        return a / b
